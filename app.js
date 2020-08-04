91449fc10f58ea35d633d16650107e349520bf26
intom <iin.arahmah99@gmail.com>
2020-08-04 16:02:35 +0700

    update file app.js

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
-var dataProduct = {
-    maximum: 50,
-    products: [
-        {
-            "id": "429",
-            "name": "Flannel M",
-            "description": "kemeja dengan kualitas premium, jahitan rapi",
-            "price": "30",
-            "image": "https://i.dlpng.com/static/png/5033832-nitro-circus-crimson-jack-long-sleeve-flannel-shirt-flannel-png-600_900_preview.png"
-        },
-        {
-            "id": "430",
-            "name": "Flannel Premium L",
-            "description": "kemeja dengan kualitas premium, jahitan rapi",
-            "price": "40",
-            "image": "https://i.dlpng.com/static/png/5033832-nitro-circus-crimson-jack-long-sleeve-flannel-shirt-flannel-png-600_900_preview.png"
-        },
-        {
-            "id": "431",
-            "name": "Flannel Premium XL",
-            "description": "kemeja dengan kualitas premium, jahitan rapi",
-            "price": "50",
-            "image": "https://i.dlpng.com/static/png/5033832-nitro-circus-crimson-jack-long-sleeve-flannel-shirt-flannel-png-600_900_preview.png"
-        },
-        {
-            "id": "431",
-            "name": "Flannel Premium XXL",
-            "description": "kemeja dengan kualitas premium, jahitan rapi",
-            "price": "65",
-            "image": "https://i.dlpng.com/static/png/5033832-nitro-circus-crimson-jack-long-sleeve-flannel-shirt-flannel-png-600_900_preview.png"
-        }
 
-    ]
-
-}
 var app = new Vue({
     el: '#app',
-    data: dataProduct
+    data: {
+        maximum: 50,
+        products: null
+    },
+    mounted: function () {
+        fetch('https://hplussport.com/api/products/order/price')
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            })
+    }
 });
