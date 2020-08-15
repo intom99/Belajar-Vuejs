f97c694ab231e613f7c9132bb0dcca33aad2df9e
intom <iin.arahmah99@gmail.com>
2020-08-15 23:05:24 +0700

    add data transition

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ var app = new Vue({
         }
     },
     methods: {
+        before: function (el) {
+            el.className = 'd-none';
+        },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function () {
+                el.className = 'row d-flex mb-3 align-item-center animate__animated animate__fadeInRight';
+            }, delay)
+
+        },
+        leave: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function () {
+                el.className = 'row d-flex mb-3 align-item-center animate__animated animate__fadeOutRight';
+            }, delay)
+        },
         addItem: function (products) {
             this.cart.push(products);
         }
